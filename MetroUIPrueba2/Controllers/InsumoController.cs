f641935cb187280bfedcae0f7ce75ff9fdb50dfb
pedrogaldames <66281858+pedrogaldames@users.noreply.github.com>
2020-07-08 19:22:18 -0400

    mod-0907-2230
    Añadi acciones a controlador insumo y formularios

--- MetroUIPrueba2/Controllers/InsumoController.cs
+++ MetroUIPrueba2/Controllers/InsumoController.cs
@@ -9,14 +9,37 @@ namespace MetroUIPrueba2.Controllers
     public class InsumoController : Controller
     {
         // GET: Insumo
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ver(int id)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult Crear()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Actualizar()
+        {
+            return View();
+        }
+        [HttpGet]
         public ActionResult Inventario()
         {
             return View();
