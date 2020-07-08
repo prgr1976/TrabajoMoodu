f3481690d06e1ae901218c5593eb4365c53de7f6
pedrogaldames <66281858+pedrogaldames@users.noreply.github.com>
2020-07-08 19:32:37 -0400

    mod-0807_21.15
    Todas las acciones para el controlador productos y creacion de formularios

--- MetroUIPrueba2/Controllers/ProductoController.cs
+++ MetroUIPrueba2/Controllers/ProductoController.cs
@@ -9,12 +9,34 @@ namespace MetroUIPrueba2.Controllers
     public class ProductoController : Controller
     {
         // GET: Producto
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ver(int id)
+        {
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Actualizar()
         {
             return View();
         }
